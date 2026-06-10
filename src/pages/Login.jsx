5b0c65f5a146a2187e8c1529b9ec5c5c72291db7
sajibkhandev <sajibkhan.mern@gmail.com>
2026-06-10 17:50:40 +0600

    style: add registration and login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,87 @@
-import React from 'react'
+import { Button, styled } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import LoginImage from "../assets/loginimage.png";
+import Image from "../components/Image";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
+const ButtonCustomize = styled(Button)({
+  padding: "20px 0px",
+  width: "60%",
+  background: "#5F35F5",
+  fontSize: "20px",
+  fontWeight: "600",
+  fontFamily: "",
+  borderRadius: "86px",
+  textTransform: "capitalize",
+  marginTop: "51px",
+  marginBottom: "31px",
+});
+const InputCustomize = styled(TextField)({
+  marginTop: "50px",
+  width: "60%",
+  border: "0px",
+
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderWidth: "1px",
+      borderRadius: "10px ",
+    },
+  },
+});
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Grid container>
+      <Grid size={6}>
+        <div className="flex justify-end h-screen items-center">
+          <div className="w-140">
+            <h2 className="text-[34px] text-[#11175D] font-bold ">
+              Login to your account!
+            </h2>
+            <div>
+              <Button sx={{padding:"12px 30px",marginTop:"20px"}} variant="outlined" startIcon={<FcGoogle />}>
+              Login with Google
+            </Button>
+            </div>
 
-export default Login
+            <InputCustomize
+              type="email"
+              id="standard-basic"
+              label="Email"
+              variant="standard"
+            />
+
+            <InputCustomize
+              type="password"
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+            />
+
+            <br />
+            <ButtonCustomize variant="contained">
+              Login to Continue
+            </ButtonCustomize>
+            <p className="ml-24 mb-10 text-base font-bold cursor-pointer text-[#03014C] ">Forget Password</p>
+            
+            <Link to="/registration">
+            <p className="ml-16 text-sm cursor-pointer text-[#03014C] font-normal">
+              Don’t have an account ?{" "}
+              <span className="text-[#EA6C00]">Sign up</span>
+            </p>
+            </Link>
+
+            
+          </div>
+        </div>
+      </Grid>
+      <Grid size={6}>
+        <Image className="w-full h-screen object-cover" src={LoginImage} />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,16 +1,61 @@
-import React from "react";
+import { Button, styled } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Image from "../components/Image";
-import SignUP from "../assets/signupimage.png";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import SignUP from "../assets/signupimage.png";
+import Image from "../components/Image";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const ButtonCustomize= styled(Button)({
+  padding: '20px 0px',
+  width:'60%',
+  background:'#5F35F5',
+  fontSize:"20px",
+  fontWeight:"600",
+  fontFamily:"",
+  borderRadius:"86px",
+  textTransform:"capitalize",
+  marginTop:"51px",
+  marginBottom:"31px"
+
+})
+const InputCustomize= styled(TextField)({
+  marginTop:"30px",
+  width:"60%",
+  border:"0px",
+ 
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderWidth: '1px',
+      borderRadius:"10px ",
+  },
+}
+ 
+ 
+
+})
 
 const Registration = () => {
+  let [name,setName]=useState("")
+  let [email,setEmail]=useState("")
+  let [password,setPassword]=useState("")
+
+  let handleSignUP=()=>{
+    if(!email){
+      console.log("give me a email");
+      
+
+    }
+    console.log(name);
+    console.log(email);
+    console.log(password);
+    
+  }
   return (
     <Grid container>
       <Grid size={6}>
         <div className="flex justify-end h-screen items-center">
-          <div className="w-130">
+          <div className="w-140">
             <h2 className="text-[34px] text-[#11175D] font-bold ">
               Get started with easily register
             </h2>
@@ -18,27 +63,38 @@ const Registration = () => {
               Free register and you can enjoy it
             </p>
 
-            <TextField
-              sx={{ width: "60%" }}
+            <InputCustomize
+            type="email"
+            onChange={(e)=>setEmail(e.target.value)}
+              value={email}
               id="outlined-basic"
               label="Email Address"
               variant="outlined"
             />
-            <TextField
-              sx={{ width: "60%" }}
+            <InputCustomize
+             type="text"
+              onChange={(e)=>setName(e.target.value)}
+             value={name}
               id="outlined-basic"
-              label="Email Address"
+              label="Ful name"
               variant="outlined"
             />
-            <TextField
-              sx={{ width: "60%" }}
+            <InputCustomize
+            type="password"
+             onChange={(e)=>setPassword(e.target.value)}
+              value={password}
               id="outlined-basic"
-              label="Email Address"
+              label="Password"
               variant="outlined"
             />
             <br />
-            <Button variant="contained">Sign up</Button>
-            <p>Already  have an account ? Sign In</p>
+            <ButtonCustomize onClick={handleSignUP} variant="contained">Sign up</ButtonCustomize>
+            <div className="w-8/12">
+            <Link to="/login">
+            <p className="text-sm cursor-pointer text-center text-[#03014C] font-normal">Already  have an account ? <span className="text-[#EA6C00]">Sign In</span></p>
+            </Link>
+              
+            </div>
 
           </div>
         </div>
