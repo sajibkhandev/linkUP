747d907bcef379d19c46a872a44ec0281c4833a5
sajibkhandev <sajibkhan.mern@gmail.com>
2026-08-10 17:55:09 +0600

    feat: add group

--- src/components/TitleList.jsx
+++ src/components/TitleList.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsThreeDotsVertical } from 'react-icons/bs'
+import { MdOutlineCancel } from "react-icons/md";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import toast, { Toaster } from "react-hot-toast";
 
-const TitleList = ({title,className}) => {
+
+const TitleList = ({title,className,addContent}) => {
+    const db = getDatabase();
+  let [popup,setPopup]=useState(false)
+  let [groupname,setGroupName]=useState("")
+
+  let handleCrateGroup=()=>{
+    set(push(ref(db, "mygroups/")), {
+          groupname:groupname,
+          groupimage:"https://i.ibb.co.com/VWNLQgbD/700702-network-512x512.png",
+          groupmember:[]
+        }).then(()=>{
+          setPopup(false)
+          toast.success("Group Created")
+        })
+    
+    
+  }
   return (
      <div className={`flex justify-between items-center ${className}`}>
+      <Toaster />
          <h3 className="text-base text-white font-semibold font-pop">{title}</h3>
+         {
+          addContent && 
+         <button onClick={()=>setPopup(true)} className='text-white bg-white/40 py-1 px-3 rounded-md text-xs'>{addContent}</button>
+         }
         <BsThreeDotsVertical className="text-[#fff] text-sm"/>
+
+
+       {
+        popup &&
+         <div className='absolute top-0 left-0 w-full h-full  z-40 flex justify-center items-center'>
+           <div className='flex flex-col gap-y-3 justify-center items-center w-[500px] py-20 bg-sky-300 rounded-md'>
+            <MdOutlineCancel onClick={()=>setPopup(false)} className='relative -top-[55px] left-[220px] text-white z-40 text-2xl' />
+
+            <h2 className='text-white text-3xl font-bold'>Group Name: </h2>
+            <input value={groupname} onChange={(e)=>setGroupName(e.target.value)} className='border border-black rounded-md py-1 px-3 w-[260px]' type="text" />
+           <div >
+             <button onClick={()=>setPopup(false)} className='text-white py-1 px-4 bg-blue-500 rounded'>Cancel</button>
+            <button onClick={handleCrateGroup} className='ml-4 text-white py-1 px-4 bg-red-500 rounded'>Create</button>
+           </div>
+           </div>
+        </div>
+       }
+
+
+
        </div>
   )
 }
--- src/layouts/MyGroupList.jsx
+++ src/layouts/MyGroupList.jsx
@@ -1,42 +1,114 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MakeProfile from '../components/MakeProfile'
 import TitleList from '../components/TitleList'
 import SearchBar from '../components/SearchBar'
 import Profile2 from "../assets/profile2.jpg";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { MdOutlineCancel } from 'react-icons/md';
 
 const MyGroupList = () => {
+  const db = getDatabase();
+  let [mygroup, setMygroup] = useState([]);
+  let [popup,setPopup]=useState(false)
+  let [friend,setFriend]=useState([])
+  useEffect(() => {
+    const mygroupRef = ref(db, "mygroups/");
+    let arr = [];
+    onValue(mygroupRef, (snapshot) => {
+      snapshot.forEach((item) => {
+        arr.push({ ...item.val(), id: item.key });
+
+
+      });
+      setMygroup(arr);
+    });
+  }, []);
+
+
+
+   useEffect(() => {
+    const friendsRef = ref(db, "userlist/");
+    let arr = [];
+    onValue(friendsRef, (snapshot) => {
+      snapshot.forEach((item) => {
+        arr.push({ ...item.val(), id: item.key });
+
+
+      });
+      setFriend(arr);
+    });
+  }, []);
+
+
+  let handleAddMember=()=>{
+    
+  }
+
+
+
+
   return (
     <div>
-        <SearchBar />
+      <SearchBar />
 
-          <div className="mt-3 py-4 px-5  rounded-[20px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#243F5F]">
-            <TitleList className="py-3" title="My Groups" />
+      <div className="mt-3 py-4 px-5  rounded-[20px] shadow-[0_4px_4px_rgba(0,0,0,0.25)] bg-[#243F5F]">
 
-            <div className="flex overflow-y-scroll  h-[170px] flex-col gap-y-3 ">
+        <TitleList addContent="Create Group" className="py-3" title="My Groups" />
+
+        <div className="flex overflow-y-scroll  h-[170px] flex-col gap-y-3 ">
+
+          {
+            mygroup.map(item => (
+
               <MakeProfile
                 mainClassName=""
-                profileImage={Profile2}
-                profileName={`Naymer Jr`}
+                profileImage={item.groupimage}
+                profileName={item.groupname}
                 profileStatus={`Follow me`}
-                buttonText={`Join`}
+                buttonText={`Add Member`}
+                onclick={()=>setPopup(true)}
               />
-              <MakeProfile
+            ))
+          }
+
+        </div>
+      </div>
+
+
+      {/* Add member Popup */}
+      {
+
+        popup &&
+
+      <div className='absolute top-0 left-0 w-full h-full  z-40 flex justify-center items-center'>
+        <div className='flex flex-col gap-y-3 justify-center items-center w-[500px] h-[500px] overflow-y-scroll bg-sky-300 rounded-md'>
+          <MdOutlineCancel onClick={()=>setPopup(false)}  className='relative -top-[55px] left-[220px] text-white z-40 text-2xl' />
+
+          <h2 className='pb-5 text-white text-3xl font-bold'>Mern 2504 </h2>
+          <p className='pb-5 text-black/50 text-xl font-semibold'>All Users </p>
+
+          <div className='w-full px-15'>
+            {
+              friend.map(item=>(
+                <MakeProfile
                 mainClassName=""
-                profileImage={Profile2}
-                profileName={`Naymer Jr`}
+                profileImage={item.profileurl}
+                profileName={item.username}
                 profileStatus={`Follow me`}
-                buttonText={`Join`}
+                onclick={()=>handleAddMember(item)}
+                buttonText={`Add`}
               />
-              <MakeProfile
-                mainClassName=""
-                profileImage={Profile2}
-                profileName={`Naymer Jr`}
-                profileStatus={`Follow me`}
-                buttonText={`Join`}
-              />
-              
-            </div>
+              ))
+            }
+           
           </div>
+          
+          
+        </div>
+      </div>
+      }
+
+
     </div>
   )
 }
